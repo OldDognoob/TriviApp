7b98e36496f3b07bcec3dcf2e1ec578ee1266349
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2021-10-07 17:49:54 +0300

    javascript-base-setup-file

---
+++ src/scripts.js
@@ -0,0 +1,4 @@
+/* find the element id = game in our html file */
+const game = document.getElementById('game')
+/*grab score display by id from html file */
+const scoreDisplay = document.getElementById('score')
